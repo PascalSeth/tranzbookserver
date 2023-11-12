5e7e850dc82b304e55f9f241e9e326220c930b41
PascalSeth <pascalelikem@gmail.com>
2023-11-12 09:29:38 +0000

    new commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+  origin: 'https://tranzbook-oirjiltem-pascal-s-projects-c7de1887.vercel.app/',
+  methods: ['POST', 'GET'],
+  credentials: true,
+}));
+app.options('*', cors()); // Add this line to handle preflight requests for all routes
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://tranzbook.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true'); // Add this line
+    next();
+});
+
+
+app.use(express.json());
+
+const connection = require('./db');
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+
+connection();
+
+app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening to ${port}...`));
+
+
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const { User,validate } = require('../models/user');
+const bcrypt =require('bcrypt');
+const router=require('express').Router();
+router.post('/',async (req,res)=>{
+    try{
+        const {error} =validate(req.body);
+        if (error)
+        return res.status(400).send({message:error.details[0].message});
+
+const user=await User.findOne({email:req.body.email});
+    if(user)
+    return res.status(409).send({message:"User with given email already exists"});
+
+    const salt=await bcrypt.genSalt(Number(process.env.SALT));
+    const hashPassword=await bcrypt.hash(req.body.password,salt);
+    await new User({
+        ...req.body,password:hashPassword
+    }).save();
+    res.status(401).send({message:"User created successfully"})
+} catch(error){
+res.status(500).send({message:"Internal Error"})
+};
+});
+
+
+
+module.exports=router
