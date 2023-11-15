f5dc004b5afdd0538ddeafa9b85ec3daf2721bce
PascalSeth <pascalelikem@gmail.com>
2023-11-15 08:54:29 +0000

    rewrite

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const connection = require('./db');
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 
-
+connection();
 app.use(cors({
-  origin: 'https://tranzbook-p619g7n5q-pascal-s-projects-c7de1887.vercel.app',
-  methods: ['POST', 'GET'],
-  credentials: true,
+  // origin: 'https://tranzbook-p619g7n5q-pascal-s-projects-c7de1887.vercel.app',
+  // methods: ['POST', 'GET'],
+  // credentials: true,
 }));
 
 app.use(express.json());
 
 
-connection();
+
 
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
